77373682c38ada24bfee797d546aacdfaeec2e4b
Savva Kodeikin <skodeykin@mobicont.ru>
2017-03-23 14:37:31 +0300

    Started working on a game controller

---
+++ src/main/java/com/imsavva/checkers/server/controller/ConsoleController.java
@@ -0,0 +1,21 @@
+package com.imsavva.checkers.server.controller;
+
+import com.imsavva.checkers.server.view.InterfaceDrawer;
+
+/**
+ * Console controller reads commands from command line and delegates them to a model.
+ *
+ * @author Savva Kodeikin
+ */
+public class ConsoleController implements Controller {
+
+    private InterfaceDrawer interfaceDrawer;
+
+    public ConsoleController() {
+
+    }
+
+    public void startGame() {
+
+    }
+}
---
+++ src/main/java/com/imsavva/checkers/server/controller/Controller.java
@@ -0,0 +1,16 @@
+package com.imsavva.checkers.server.controller;
+
+import com.imsavva.checkers.server.view.InterfaceDrawer;
+
+/**
+ * Game controller.
+ *
+ * @author Savva Kodeikin
+ */
+public interface Controller {
+
+    /**
+     * Let the fun begin. Start a new game.
+     */
+    void startGame();
+}
